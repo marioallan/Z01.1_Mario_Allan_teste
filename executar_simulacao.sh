bd9296f6e9a083fccc84673b8d4dbd2128959e85
Mário Allan <marioallan.la@gmail.com>
2020-12-07 22:27:46 -0300

    Novo arquivo

---
+++ executar_simulacao.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export VUNIT_SIMULATOR=modelsim
+export VUNIT_MODELSIM_PATH=$HOME/modelsim_ase/bin/
+
+##############################################
+# INSERIR AQUI !!!
+###############################################
+#script:
+  ## Instal Z01.1 tools
+  #bash updateZ01tools.sh
+
+  ## run scripts
+  #python3 Projetos/A-AmbienteDesenvolvimento/testeAmbienteDesenvolvimento.py
+  python3 Projetos/B-LogicaCombinacional/testeLogicaCombinacional.py
